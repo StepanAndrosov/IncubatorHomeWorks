09fecfefb0413f5abfad1e61b50e9961a6ae7489
Androsov-Stepan <stapan.news@yandex.ru>
2021-10-20 10:10:25 +0300

    Homework 5: gh-pages, router-dom, header for pre-jun page

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import style from "./Header.module.scss"
 
 function Header() {
-    return (
-        <div>
-            // add NavLinks
 
+    const [active, setActive] = useState(false)
+    const isActive = active ? `${style.navBlock} ${style.active}` : style.navBlock
+    const switchLinks = () => {
+        setActive(!active)
+    }
+
+    return (
+        <div className={style.Header}>
+            <span onClick={switchLinks} className={style.spanClick}>Click!</span>
+            <div className={isActive}>
+           <NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink>
+            <NavLink to={PATH.JUNIOR}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>Junior+</NavLink>
+            </div>
         </div>
     )
 }
